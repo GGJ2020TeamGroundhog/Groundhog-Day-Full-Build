a83d17bcd660dd7bba2f783eb3c0730033e5d339
StoneKnight117 <60109362+StoneKnight117@users.noreply.github.com>
2020-02-02 10:54:08 -0500

    Add files via upload

---
+++ InteractableObstacles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InteractableObstacles : Interactables
+{
+    bool isFixed = false;
+    public string tool;
+    public string dialogueMessage;
+
+    public override void Interact(string toolUsed) {
+        if (tool == toolUsed) {
+            isFixed = true;
+            Debug.Log("You fixed obstacle!");
+        }
+        else if(tool == "noTool") {
+            isFixed = true;
+        }
+    }
+}
