af5b5d5c4ffd474b47d664c84ffaf8934f581fd9
jihyun12 <37644129+jihyun12@users.noreply.github.com>
2020-02-02 14:22:31 -0500

    Main menu

---
+++ START.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class START : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("HubLevel");
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+ 
+    }
+}
+
